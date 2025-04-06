9bad676107ae353b76b0632f879c7907b296fb8f
sanalkumars <sanalskumar126@gmail.com>
2025-04-06 11:25:12 +0530

    url error fixed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,6 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client"
 
+const BASE_URL =import.meta.env.MODE==="development"? import.meta.env.VITE_BASE_URL :"/"
 export const useAuthStore = create((set , get)=>({
     authUser: null,
     isSigningUp :false,
@@ -89,7 +90,7 @@ export const useAuthStore = create((set , get)=>({
 
     connectSocket: () => {
         const { authUser } = get();
-        console.log("baseurl" , import.meta.env.VITE_BASE_URL);
+        console.log("baseurl" , );
         if (!authUser || get().socket?.connected) return;
     
         const socket = io(import.meta.env.VITE_BASE_URL, {
